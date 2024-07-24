a250156b6393403fe47a5dc776833639355678b7
Devansh Vishwakarma <devwish0207@gmail.com>
2024-07-24 22:41:57 +0530

    Login and signup page

---
+++ frontend/notes-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../src/pages/Home/Home'
+import Login from '../src/pages/Login/Login'
+import SignUp from '../src/pages/SignUp/SignUp'
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
+    </Routes>
+  </Router>
+)
+const App = () => {
+  return (
+    <div>
+      {routes}
+    </div>
+  )
+}
+
+export default App
